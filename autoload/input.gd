ab3ebac08ea1283bf1338f83197a2836a0e1bf8a
mengo329 <115123539+mengo329@users.noreply.github.com>
2025-04-25 23:52:18 -0400

    Input display!

---
+++ autoload/input.gd
@@ -0,0 +1,54 @@
+extends Node2D
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_up"):
+		$DPad/Up.visible = true
+	else:
+		$DPad/Up.visible = false
+	if Input.is_action_pressed("move_down"):
+		$DPad/Down.visible = true
+	else:
+		$DPad/Down.visible = false
+	if Input.is_action_pressed("move_left"):
+		$DPad/Left.visible = true
+	else:
+		$DPad/Left.visible = false
+	if Input.is_action_pressed("move_right"):
+		$DPad/Right.visible = true
+	else:
+		$DPad/Right.visible = false
+	
+	if Input.is_action_pressed("dash"):
+		$ButtonA/Pressed.visible = true
+	else:
+		$ButtonA/Pressed.visible = false
+		
+	if Input.is_action_pressed("shoot"):
+		$ButtonB/Pressed.visible = true
+	else:
+		$ButtonB/Pressed.visible = false
+		
+	if Input.is_action_pressed("jump"):
+		$ButtonC/Pressed.visible = true
+	else:
+		$ButtonC/Pressed.visible = false
+		
+	if Input.is_action_pressed("switch_left"):
+		$ButtonX/Pressed.visible = true
+	else:
+		$ButtonX/Pressed.visible = false
+		
+	if Input.is_action_pressed("switch_right"):
+		$ButtonY/Pressed.visible = true
+	else:
+		$ButtonY/Pressed.visible = false
+		
+	if Input.is_action_pressed("buster"):
+		$ButtonZ/Pressed.visible = true
+	else:
+		$ButtonZ/Pressed.visible = false
+	
+	if Input.is_action_pressed("start"):
+		$Start/Pressed.visible = true
+	else:
+		$Start/Pressed.visible = false
