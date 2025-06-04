b360e689732a246a1c27816f47026d0a368de8d3
mengo329 <115123539+mengo329@users.noreply.github.com>
2025-06-03 21:30:49 -0400

    updated CR's double fin shredder. Messed with eweapom get

--- scenes/objects/players/weapons/copy_robot/cr_fin_shredder.gd
+++ scenes/objects/players/weapons/copy_robot/cr_fin_shredder.gd
@@ -1,54 +1,45 @@
 extends CharacterBody2D
 
 var W_Type = GameState.DMGTYPE.CR_SHARK2
-var dying : bool
+var dying : bool = false
 var spins : int
 var detectpos
+var going : bool = false
 
 func _ready():
 	$SpawnSound.play()
-	$AnimatedSprite2D.play("Generate")
 	velocity.y = 0
-	detectpos = GameState.playerposy
+	detectpos = GameState.player.position.y
 		
 		
 func _physics_process(_delta):
 	move_and_slide()
 	
+	if !is_on_floor() and $Projectile.visible == true:
+		$Projectile2.visible = true
+	else:
+		$Projectile2.visible = false
+		
+				
+	
 	if GameState.player != null:
-		$AnimatedSprite2D.material.set_shader_parameter("palette", GameState.player.get_node("Sprite2D").material.get_shader_parameter("palette"))
+		$Projectile.material.set_shader_parameter("palette", GameState.player.get_node("Sprite2D").material.get_shader_parameter("palette"))
+		$Spinner.material.set_shader_parameter("palette", GameState.player.get_node("Sprite2D").material.get_shader_parameter("palette"))
 	
 	if GameState.current_weapon != GameState.WEAPONS.SHARK:
 		queue_free()
-	
-	if ($AnimatedSprite2D.animation == "Generate") and ($AnimatedSprite2D.get_frame() == 2):
-		spins += 1
-		velocity.x = velocity.x * 1.5
-		if spins > 3:
-			if detectpos == GameState.playerposy:
-				velocity.x = velocity.x * 74
-				$SpawnSound.play()
-				$AnimatedSprite2D.play("Copy")
-				GameState.weapon_energy[GameState.WEAPONS.SHARK] -= 2
-		if spins == 10:
-			queue_free()
 		
-	
-	if ($AnimatedSprite2D.animation == "Copy") and ($AnimatedSprite2D.get_frame() == 4):
-		velocity.x = velocity.x * 4
-		$AnimatedSprite2D.play("Copy-loop")
+	if $Projectile.animation == "Copy" and $Projectile.frame == 4:
+		$Projectile.play("Copy-loop")
+		$Projectile2.play("Copy-loop")
 	
 	if dying == true:
-		if ($AnimatedSprite2D.get_frame() == 0):
+		if ($Projectile.get_frame() == 0):
 			velocity.x = velocity.x * 0.7
 		velocity.x = velocity.x * 0.8
-		if ($AnimatedSprite2D.get_frame() == 1):
+		if ($Projectile.get_frame() == 1):
 			GameState.onscreen_sp_bullets = 0
 			queue_free()
-	
-	
-	if velocity.x == 0:
-		destroy()
 	
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
@@ -58,7 +49,8 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 func destroy():
 	$HitSound.play()
 	velocity.y = 0
-	$AnimatedSprite2D.play("Copy-hit")
+	$Projectile.play("Copy-hit")
+	$Projectile2.play("Copy-hit")
 	dying = true
 	
 	
@@ -67,4 +59,24 @@ func kill():
 	pass
 
 func reflect():
+	pass
+
+func _on_spinner_animation_looped() -> void:
+	if going == false:
+		spins += 1
+		velocity.x *= 1.9
+		if spins > 2:
+			if detectpos == GameState.player.position.y:
+				velocity.x = velocity.x * 344
+				$SpawnSound.play()
+				$Spinner.visible = false
+				$Projectile.visible = true
+				$Projectile.play("Copy")
+				$Projectile2.play("Copy")
+				going = true
+				GameState.weapon_energy[GameState.WEAPONS.SHARK] -= 2
+		if spins == 10 and going == false:
+			queue_free()
+
+func _on_projectile_animation_finished() -> void:
 	pass
